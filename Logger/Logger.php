fee656a43181c8d0e82adcd8555277c653ed3bee
Martin Georgiev <martin.georgiev@gmail.com>
2017-10-25 14:20:48 +0100

    Serialize the errored value as we'd like to catch arrays and other unexpected data types

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -2,40 +2,36 @@
 
 namespace Oh\FormErrorLogBundle\Logger;
 
+use Monolog\Logger;
 use Oh\FormErrorLogBundle\Logger\ErrorLogInterface;
-use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 
 class Logger implements ErrorLogInterface
 {
     /**
-     * The monolog logger
-     * @var Monolog\Logger
+     * @var Logger
      */
     private $logger;
-    
+
     public function __construct($logger)
     {
         $this->logger = $logger;
     }
-    
+
     /**
-     * 
      * @param string $formName
      * @param string $key
      * @param string $error
      * @param string $value
+     * @return void
      */
     public function log($formName, $key, $error, $value = '', $uri = '')
     {
-        
-        $this->logger->notice(strtr('%0 - Error in form "%1" in position "%2": "%3" with value "%4"', array(
-            '%0'=>$uri,
-            '%1'=>$formName,
-            '%2'=>$key,
-            '%3'=>$error,
-            '%4'=>$value
+        $this->logger->notice(strtr('%0 - Error in form "%1" in position "%2": "%3" with serialized value "%4"', array(
+            '%0' => $uri,
+            '%1' => $formName,
+            '%2' => $key,
+            '%3' => $error,
+            '%4' => serialize($value),
         )));
-        
     }
-    
 }
